45f4632e9aa0f84546d89c6e332bc5ed3062c02d
Vitor Baraçal Guimarães <you@example.com>
2024-11-20 20:35:26 -0300

    ninth commit at 19.11.2024

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,32 @@
+// Login() function activated when the user clicks the login button
 function Login() {
+    // Stores the value of the respective input in their respective variables
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
 
+    // Shows variable values ​​in the console
+    console.log(name);
+    console.log(email);
+    console.log(password);
+
+    // Checks the email if it is in the proper format
+    if (!email.includes("@") || !email.includes(".")) {
+        alert("Por favor, insira um e-mail válido. Um e-mail precisa ter '@' e um domínio como '.com'.");
+        return;
+    }
+    
+    // Checks the password if it's in the appropriate format (8 digits and a special character)
+    if (password.length < 8 || !password.includes("!") && !password.includes("@") && !password.includes("#") && !password.includes("$") && !password.includes("%") && !password.includes("&") && !password.includes("*")) {
+        alert("A senha deve ter no mínimo 8 caracteres e incluir pelo menos um caractere especial (!, @, #, $, %, &, *).");
+        return;
+    }
+
+    // Shows an alert to start user registration
     alert("Ops! Parece que você não possuí cadastro conosco! Clique em OK para prosseguir")
 
+    
+    // Store the new HTML of the page in a variable
     const formadd = `
     <h2> Sign Up on Website </h2>
     <form action="" method="post">  
@@ -36,23 +58,24 @@ function Login() {
         </div>   
         <button type="submit" class="button" onclick="Cadastro()">Login</button>
             
-    </form>
-    ` ;
-
-    document.getElementById('main').innerHTML = formadd;
-
-    document.getElementById('name').value = name;
-    document.getElementById('email').value = email;
-    document.getElementById('password').value = password;
+    </form> `;
 
     
+    // adds the new html to the chosen id
+    document.getElementById('main').innerHTML = formadd;
+
+    // Keeps the user's fields already filled in
+    document.getElementById('name').value = name;
+    document.getElementById('email').value = email;
+    document.getElementById('password').value = password;    
 }
+
+// Registration() function activated when the user clicks the login button after passing the first step
 function Cadastro(){
+    // Displays an alert saying that the registration has been completed
     alert ("Cadastro realizado com sucesso");
-
+    
+    // Returns to the website home page
     window.location.href = "index.html";
-
-
-
 }
 
